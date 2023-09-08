b0b8ed7e238dbc88df84ce8fa6626c925d3152f9
rohitchauhan533 <chauhanrohit533@gmail.com>
2023-09-08 22:50:21 +0530

    application completion

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
-import './App.css';
+import "./App.css";
 import React, { useEffect, useState } from "react";
-import Weather from './components/Weather';
+import Weather from "./components/Weather";
 import {
   REACT_APP_WEATHER_API_KEY,
   REACT_APP_WEATHER_API_URL,
 } from "./../env.config.js";
+import { Dimmer, Loader } from "semantic-ui-react";
 export default function App() {
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
+  const [lat, setLat] = useState("28.8978");
+  const [long, setLong] = useState("76.6297");
   const [data, setData] = useState([]);
 
   const fetchData = async () => {
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition(function (position) {
       setLat(position.coords.latitude);
       setLong(position.coords.longitude);
     });
+    const weatherUrl =  `${REACT_APP_WEATHER_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${REACT_APP_WEATHER_API_KEY}`;
 
-    await fetch(`${REACT_APP_WEATHER_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${REACT_APP_WEATHER_API_KEY}`)
-    .then(res => res.json())
-    .then(result => {
-      setData(result)
-      console.log(result);
-    });
-  }
+    await fetch(
+      weatherUrl
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result);
+        console.log(result);
+      });
+  };
 
   useEffect(() => {
-
     fetchData();
-  }, [lat,long])
+  }, [lat, long]);
   return (
     <div className="App">
-      {(typeof data.main !== 'undefined') ? (
-        <Weather weatherData={data}/>
-      ): (
-        <div></div>
+      {(typeof data.main != "undefined") ? (
+        <Weather weatherData={data} />
+      ) : (
+        <div>
+          <Dimmer active>
+            <Loader>Loading..</Loader>
+          </Dimmer>
+        </div>
       )}
     </div>
   );
