c12c5929112079b6cde239d85268752c824a655b
rohitchauhan533 <chauhanrohit533@gmail.com>
2023-09-08 21:35:23 +0530

    api data fetch

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
 import './App.css';
-import React, {useEffect, useState} from "react";
-import Weather from  "./components/weather"
-import "dotenv/config";
-
-// console.log(process);
-function App() {
+import React, { useEffect, useState } from "react";
+//import Weather from './components/weather';
+import {
+  REACT_APP_WEATHER_API_KEY,
+  REACT_APP_WEATHER_API_URL,
+} from "./../env.config.js";
+export default function App() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        setLat(position.coords.latitude);
-        setLong(position.coords.longitude);
-      });
+  const fetchData = async () => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
 
-      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-      .then(res => res.json())
-      .then(result => {
-        setData(result)
-        console.log(result);
-      });
-    }
+    await fetch(`${REACT_APP_WEATHER_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${REACT_APP_WEATHER_API_KEY}`)
+    .then(res => res.json())
+    .then(result => {
+      setData(result)
+      console.log(result);
+    });
+  }
+
+  useEffect(() => {
+
     fetchData();
   }, [lat,long])
   return (
     <div className="App">
-      {(typeof data.main != 'undefined') ? (
-        <Weather weatherData={data}/>
-      ): (
-        <div></div>
-      )}
-      
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Weather.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+import moment from 'moment';
+
+const CardExampleCard = ({weatherData}) => (
+  <div className="main">
+      <p className="header">{weatherData.name}</p>
+      <div>
+        <p className="day">Day: {moment().format('dddd')}</p>
+      </div>
+
+      <div>
+        <p className="temp">Temprature: {weatherData.main.temp} &deg;C</p>
+      </div>
+  </div>
+)
+
+export default CardExampleCard;
